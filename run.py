f1d2ffc36dac4f962aa12708b8118ffd09e81021
sonetto104 <peter_regan88@yahoo.com>
2023-02-01 13:11:54 +0000

    Print welcome message. Also checking that py file is working.

--- run.py
+++ run.py
@@ -11,3 +11,5 @@ CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("UT2 Tracker Spreadsheet")
+
+print("Welcome to Unstoppable UT2, where you can keep track of your UT2 performance. In case you're unfamiliar with the term 'UT2', it roughly means an aerobic workout at an intensity which can be held for the full workout duration. You should be comfortable enough to speak and be operating at 65-75% maximimum heart rate. The workout should last approximately 60 minutes.")
