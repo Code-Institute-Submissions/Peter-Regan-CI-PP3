39ace78fdb1897ee9d70c29e1dcb00f68baaa642
sonetto104 <peter_regan88@yahoo.com>
2023-02-02 11:37:14 +0000

    Define function that can build brand new spreadsheet and test that it works.

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 import gspread 
 from google.oauth2.service_account import Credentials
+# The code on line 4 was taken from this url:
+# https://stackoverflow.com/questions/67551298/using-python-to-create-a-new-google-sheet
+from gspread_dataframe import set_with_dataframe
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,4 +15,14 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("UT2 Tracker Spreadsheet")
 
-print("Welcome to Unstoppable UT2, where you can keep track of your UT2 performance. In case you're unfamiliar with the term 'UT2', it roughly means an aerobic workout at an intensity which can be held for the full workout duration. You should be comfortable enough to speak and be operating at 65-75% maximimum heart rate. The workout should last approximately 60 minutes.")
+print("Welcome to Unstoppable UT2, where you can keep track of your UT2 performance. In case you're unfamiliar with the term 'UT2', it refers to an aerobic workout at an intensity which can be held for the full workout duration. You should be comfortable enough to speak and be operating at 65-75% maximimum heart rate. The workout should last approximately 60 minutes.")
+
+def create_new_sheet():
+    """
+    Creates new spreadsheet in Google Sheets.
+    """
+    sh = GSPREAD_CLIENT.create('A new spreadsheet')
+    sh.share('regan.peter.w@gmail.com', perm_type='user', role='writer')
+
+
+create_new_sheet()
