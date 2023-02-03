2f932ac6af25bf2b3704d7e3262685d599d94f76
sonetto104 <peter_regan88@yahoo.com>
2023-02-03 12:38:45 +0000

    Add function that can search for files in Google Drive.

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
+from __future__ import print_function
 import gspread 
 from google.oauth2.service_account import Credentials
 # The code on line 4 was taken from this url:
 # https://stackoverflow.com/questions/67551298/using-python-to-create-a-new-google-sheet
 from gspread_dataframe import set_with_dataframe
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -30,8 +35,6 @@ def type_username():
 
 
 username = type_username()
-
-def check_if_user_exists():
     
 
 
@@ -47,8 +50,45 @@ create_new_user_sheet()
 
 
 
+def search_file():
+    """Search file in drive location
+
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.
+    """
+    creds = CREDS
+
+    try:
+        # create drive api client
+        service = build('drive', 'v3', credentials=creds)
+        files = []
+        page_token = None
+        while True:
+            # pylint: disable=maybe-no-member
+            response = service.files().list(q="name contains 'UT2'",
+                                            spaces='drive',
+                                            fields='nextPageToken, '
+                                                   'files(id, name)',
+                                            pageToken=page_token).execute()
+            for file in response.get('files', []):
+                # Process change
+                print(F'Found file: {file.get("name")}, {file.get("id")}')
+            files.extend(response.get('files', []))
+            page_token = response.get('nextPageToken', None)
+            if page_token is None:
+                break
+
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+        files = None
+
+    return files
 
 
+if __name__ == '__main__':
+    search_file()
+    
 
 
 
