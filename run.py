0fec98ce47a127f426346b7ab6c5065fecf3d7d2
sonetto104 <peter_regan88@yahoo.com>
2023-02-06 11:57:08 +0000

    Add env file in order to store my email address privately, which is being used in order to access google drive storage holding user data.

--- run.py
+++ run.py
@@ -1,13 +1,16 @@
 from __future__ import print_function
+import google.auth
 import gspread 
+import os
+from dotenv import load_env
 from google.oauth2.service_account import Credentials
 # The code on line 4 was taken from this url:
 # https://stackoverflow.com/questions/67551298/using-python-to-create-a-new-google-sheet
 from gspread_dataframe import set_with_dataframe
-import google.auth
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+load_env()
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -19,6 +22,7 @@ CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("UT2 Tracker Spreadsheet")
+email_address = os.getenv("EMAIL_ADDRESS")
 
 print("Welcome to Unstoppable UT2, where you can keep track of your UT2 performance. In case you're unfamiliar with the term 'UT2', it refers to an aerobic workout at an intensity which can be held for the full workout duration. You should be comfortable enough to speak and be operating at 65-75% maximimum heart rate. The workout should last approximately 60 minutes.")
 
@@ -34,8 +38,8 @@ def type_username():
 
 
 username = type_username()
-    
 
+    
 def create_new_user_sheet():
     """
     Creates new spreadsheet in Google Sheets.
@@ -64,7 +68,7 @@ def search_file():
         page_token = None
         while True:
             # pylint: disable=maybe-no-member
-            response = service.files().list(q="name contains 'UT2 Spreadsheet Tracker'",
+            response = service.files().list(q="name contains",
                                             spaces='drive',
                                             fields='nextPageToken,'
                                                    'files(id, name)',
